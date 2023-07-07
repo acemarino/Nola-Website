91075f2a2aa3127c64f9009d75df7ead21e317c5
acemarino <ace.marino@gmail.com>
2023-07-07 15:19:59 -0400

    creating gallery
    adding temp images to gallery

---
+++ nola-website/src/pages/gallery.js
@@ -0,0 +1,29 @@
+
+function Gallery(){
+    return(
+        <>
+       <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+        <div >
+            <div id='header'>
+                This is the Gallery
+            </div>
+            
+        </div>
+        
+        <div class="grid" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 160 }'>
+        <div class="grid-sizer"></div>
+            <div class="grid-item">
+            <img src="./20230707_112047.jpg" alt="bug"  />
+            </div> 
+            <div class="grid-item">
+            <img src="./20230701_194324.jpg" alt="bug"  />
+            </div>   
+            <div class="grid-item">
+            <img src="./20230706_163905.jpg" alt="bug" />
+            </div>      
+        </div>
+       </> 
+    );
+        
+}
+export default Gallery;
--- nola-website/src/pages/home.js
+++ nola-website/src/pages/home.js
@@ -1,13 +1,18 @@
+import Gallery from "./gallery";
 function Home(){
     return(
+        <>
         <div >
             <div id='header'>
                 This is the home page
             </div>
+            
             <div id='bulk'>
                 smaller text
             </div>
         </div>
+        <Gallery />
+        </>
     );
         
 }
