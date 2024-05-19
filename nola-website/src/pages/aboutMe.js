658cd1064d166d73fa641816f8d1b968f5549274
acemarino <ace.marino@gmail.com>
2024-05-19 16:46:25 -0400

    social link adjusments

--- nola-website/src/pages/aboutMe.js
+++ nola-website/src/pages/aboutMe.js
@@ -4,10 +4,20 @@ import { Junior} from './images';
 import portrait from "./images/Senior/SrSelfPortrait.jpg"
 import insta from "./images/Other/insta.png"
 import insta2 from "./images/Other/insta2.png"
+import inprint from "./images/Other/inprnt.svg"
 function About(){
     return(
 
         <div className="aboutPage">
+          
+            
+            <div className="pictSide">
+                
+                <img src={portrait} alt="Logo" className='aboutImg'></img>
+                <div>
+                    
+                </div>
+            </div>
             <div className="txtSide">
                 <div id='header'>
                     About the Artist
@@ -16,22 +26,17 @@ function About(){
                     Nola (they/them) is a 18-year-old artist with an interest in illustration and design. They primarily work digitally (Procreate and Clip Studio Paint) or use acrylic paint for their work. Their recent work focuses on storytelling and illustration through the use of their original characters. In the past, they’ve explored childhood and nostalgia in their work, still drawing from those inspirations today.
                 
                 </div>
-                
-            </div>
-            
-            <div className="pictSide">
-                <div>
-                    <img src={insta} alt="Logo" className='instaIcon'></img>
-                    <a href="https://www.instagram.com/granola.815/?hl=en" target="_blank" id='tag'>@granola.815</a>
-                   
-                </div>
-                <img src={portrait} alt="Logo" className='aboutImg'></img>
-                <div>
-                    <img src={insta} alt="Logo" className='instaIcon'></img>
-                    <a href="https://www.instagram.com/art.by.granola/?hl=en" target="_blank" id='tag'>@art.by.granola</a>
+                <div className='Links'>
+                    <div>
+                        <img src={insta} alt="Logo" className='insta2Icon'></img>
+                        <a href="https://www.instagram.com/art.by.granola/?hl=en" target="_blank" className='tag' >@art.by.granola</a>
+                    </div>  
+                    <div>
+                        <img src={inprint} alt="Logo" className='inprintIcon'></img>
+                        <a href="https://www.inprnt.com/gallery/art.by.granola/" target="_blank" className='tag' >@art.by.granola</a>
+                        </div> 
                 </div>
             </div>
-          
         </div>
     );
         
